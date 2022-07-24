587d86879620f6eea05c45e7a946f0fd8cae8bab
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-07-24 10:45:11 +0900

    feat: Add Components

--- src/Components/Recruit/NavBar/NavBar.js
+++ src/Components/Recruit/NavBar/NavBar.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./NavBar.css";
 
 function NavBar() {
+  // 마우스 윗부분에 올리면 네비게이션바 뜨는 기능 구현해야 함! 근데 어려움
   return (
     <div>
-      <p className="sub-nav-home">Home</p>
-      <p className="sub-nav-process">process</p>
-      <p className="sub-nav-FAQ">FAQ</p>
-      <p className="sub-nav-recruit">recruit</p>
-      <div className="sub-nav-btn">Log in</div>
+      <NavBarContents />
     </div>
   );
 }
 
+function NavBarContents() {
+  return (
+    <div className="sub-nav-bg">
+      <span className="sub-nav-home">Home</span>
+      <span className="sub-nav-process">process</span>
+      <span className="sub-nav-FAQ">FAQ</span>
+      <span className="sub-nav-recruit">recruit</span>
+      <div className="sub-nav-btn">Log in</div>
+    </div>
+  );
+}
 export default NavBar;
