83d0622b64635526be75a61cf208c40dfefb6d1f
yonsoo <ynsoo.seo@gmail.com>
2022-07-24 22:23:32 +0900

    docs: App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import MainHome from "./Components/Main/pages";
 
 function App() {
   return (
-    <Router>
-      <MainHome />
-    </Router>
+    <></>
+    // <Router>
+    //   <MainHome />
+    // </Router>
   );
 }
 
 export default App;
+
+// App.js는 상대꺼 주석처리하고 걍 쓰는걸로.
