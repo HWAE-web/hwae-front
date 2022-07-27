1e739d3a573ea14a996a5acf2c51040bd019c6e8
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-07-28 02:08:28 +0900

    임시

--- src/Components/Recruit/HomePage/Home.js
+++ src/Components/Recruit/HomePage/Home.js
@@ -1,25 +1,36 @@
-import "./Home.css";
 import NavBar from "../NavBar/NavBar.js";
 import hwae_3d from "../../../Images/hwae-3d.svg";
 import RR from "../../../Images/RR.svg";
+import { GlobalFonts } from "../../../fonts/fonts";
+import {
+  subHomeBg,
+  subHomeTitle,
+  subHomeTitle2,
+  subHomeBtn,
+  subHomeImg,
+  subHomeRR,
+} from "./HomeElement.js";
 
 function Home() {
   return (
-    <div className="sub-home-bg">
-      <NavBar />
-      <p className="sub-home-title">
-        HWA-E <br />
-        2nd Member
-        <br /> Recruiting
-      </p>
-      <p className="sub-home-title2">
-        화이의 두번째 화이어를 모집합니다. <br />
-        화이에서 함께할 분들은 지원 부탁드립니다!
-      </p>
-      <div className="sub-home-button">Read More</div>
-      <img src={hwae_3d} alt="hwae_3d" className="sub-home-img"></img>
-      <img src={RR} alt="RR" className="sub-home-RR"></img>
-    </div>
+    <>
+      <GlobalFonts />
+      <subHomeBg>
+        <NavBar />
+        <subHomeTitle>
+          HWA-E <br />
+          2nd Member
+          <br /> Recruiting
+        </subHomeTitle>
+        <subHomeTitle2>
+          화이의 두번째 화이어를 모집합니다. <br />
+          화이에서 함께할 분들은 지원 부탁드립니다!
+        </subHomeTitle2>
+        <subHomeBtn>Read More</subHomeBtn>
+        <subHomeImg src={hwae_3d}></subHomeImg>
+        <subHomeRR src={RR}></subHomeRR>
+      </subHomeBg>
+    </>
   );
 }
 
---
+++ src/Components/Recruit/HomePage/HomeElement.js
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+export const subHomeBg = styled.div`
+  position: absolute;
+  width: 1920px;
+  height: 1080px;
+  left: 0px;
+  top: 0px;
+
+  background: #f8f8fa;
+`;
+export const subHomeTitle = styled.div`
+  position: absolute;
+  width: 699px;
+  height: 348px;
+  left: 185px;
+  top: 225px;
+
+  font-family: "Roboto Bold";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 96px;
+  line-height: 105%;
+
+  margin: 0px;
+
+  color: #333333;
+`;
+export const subHomeTitle2 = styled.div`
+  position: absolute;
+  width: 457px;
+  height: 72px;
+  left: 190px;
+  top: 573px;
+
+  font-family: "Noto Sans Regular";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 150%;
+
+  letter-spacing: -0.008em;
+  margin: 0px;
+
+  color: #666666;
+`;
+
+export const subHomeBtn = styled.div`
+  position: absolute;
+  left: 9.95%;
+  right: 82.55%;
+  top: 62.96%;
+  bottom: 32.59%;
+
+  background: #ff2d46;
+  box-shadow: 0px 2.80141px 11.2057px rgba(255, 45, 70, 0.2);
+  border-radius: 52.293px;
+
+  font-family: "Poppins SemiBold";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: #ffffff;
+`;
+export const subHomeImg = styled.img`
+  position: absolute;
+  width: 1160px;
+  height: 845px;
+  left: 785px;
+  top: 57px;
+`;
+export const subHomeRR = styled.img`
+  position: absolute;
+  left: 48.96%;
+  right: 48.96%;
+  top: 90.65%;
+  bottom: 5.65%;
+`;
