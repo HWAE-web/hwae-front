d5319d3c1f084ed5f6465cd43849b56dea759dc3
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-07-28 02:30:31 +0900

    feat: add GlobalFonts

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,12 +1,63 @@
 import { createGlobalStyle } from "styled-components";
-import Poppins from "../fonts/Poppins-Medium.woff";
 
-export default createGlobalStyle`
- @font-face {
-       font-family: 'Poppins' ;
-       src: local('Poppins'),
-       url('../fonts/Poppins-Medium.woff') format('woff');
-       font-weight: 300;
-       font-style: normal;
- }
+export const GlobalFonts = createGlobalStyle`
+      @font-face {
+            font-family: 'Roboto Regular';
+            src: url('/src/fonts/Roboto-Regular.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Roboto Medium';
+            src: url('/src/fonts/Roboto-Medium.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Roboto Bold';
+            src: url('/src/fonts/Roboto-Bold.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Poppins SemiBold';
+            src: url('/src/fonts/Poppins-SemiBold.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Poppins Bold';
+            src: url('/src/fonts/Poppins-Bold.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Poppins Medium';
+            src: url('/src/fonts/Poppins-Medium.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Noto Sans Bold';
+            src: url('/src/fonts/NotoSans-Bold.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Noto Sans Regular';
+            src: url('/src/fonts/NotoSans-Regular.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Raleway Bold';
+            src: url('/src/fonts/Raleway-Bold.ttf') format('truetype');
+      }
+      
+      @font-face {
+            font-family: 'Inter Medium';
+            src: url('/src/fonts/Inter-Medium.ttf') format('truetype');
+      }
+
+      @font-face {
+            font-family: 'Spoqa Han Sans Neo Bold';
+            src: url('/src/fonts/SpoqaHanSansNeo-Bold.ttf') format('truetype');
+      }
+      
+      @font-face {
+            font-family: 'Spoqa Han Sans Neo Regular';
+            src: url('/src/fonts/SpoqaHanSansNeo-Regular.ttf') format('truetype');
+      }
 `;
