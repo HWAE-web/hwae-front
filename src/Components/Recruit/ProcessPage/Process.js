4d68c5fa7a146c7b8c930c601088708868ef618d
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-07-29 00:35:58 +0900

    feat: use styled-components

--- src/Components/Recruit/ProcessPage/Process.js
+++ src/Components/Recruit/ProcessPage/Process.js
@@ -1,41 +1,53 @@
-import "./Process.css";
+import { GlobalFonts } from "../../../fonts/fonts";
 import calendar from "../../../Images/calendar.svg";
+import {
+  SubProcessBg,
+  SubProcessMainTitle,
+  SubProcessMainDetail,
+  SubProcessFirst,
+  SubProcessSecond,
+  SubProcessThird,
+  SubProcessFourth,
+  SubProcessTitle,
+  SubProcessTitle1,
+  SubProcessDetail,
+  SubProcessCalendar,
+} from "./ProcessElement.js";
 function Process() {
   return (
-    <div className="sub-process-bg">
-      <p className="sub-process-title">Recruiting Process</p>
-      <p className="sub-process-detail">
+    <SubProcessBg>
+      <GlobalFonts />
+      <SubProcessMainTitle>Recruiting Process</SubProcessMainTitle>
+      <SubProcessMainDetail>
         HWA-E는 6개월 정도의 기간 동안 디자이너와 개발자가 팀을 이루어
         아이디어를 구체 화하고 프로젝트를 진행합니다. 사업 화보다는 즐겁게
-        서비스를 개발해 보는 경험을 얻는 것을 목적으로 하고 있습니다.{" "}
-      </p>
-      <img src={calendar} alt="calendar" className="sub-process-calendar" />
-      <div className="sub-process-first">
-        <div className="sub-process-first1">01</div>
-        <div className="sub-process-firstTitle">서류 접수</div>
-        <div className="sub-process-firstDetail">
+        서비스를 개발해 보는 경험을 얻는 것을 목적으로 하고 있습니다.
+      </SubProcessMainDetail>
+      <SubProcessCalendar src={calendar} alt="calendar" />
+      <SubProcessFirst>
+        <SubProcessTitle>01</SubProcessTitle>
+        <SubProcessTitle1>서류 접수</SubProcessTitle1>
+        <SubProcessDetail>
           2022. 11. 11. (월) ~ 11. 11. (월)
-          <br /> 23:59:59 접수 마감{" "}
-        </div>
-      </div>
-      <div className="sub-process-second">
-        <div className="sub-process-second1">02</div>
-        <div className="sub-process-secondTitle">서류 결과 발표</div>
-        <div className="sub-process-secondDetail">2022. 11. 11. (월) </div>
-      </div>
-      <div className="sub-process-third">
-        <div className="sub-process-third1">03</div>
-        <div className="sub-process-thirdTitle">면접</div>
-        <div className="sub-process-thirdDetail">
-          2022. 11. 11. (월) ~ 11. 11. (월)
-        </div>
-      </div>
-      <div className="sub-process-fourth">
-        <div className="sub-process-fourth1">04</div>
-        <div className="sub-process-fourthTitle">최종 결과 발표</div>
-        <div className="sub-process-fourthDetail">2022. 11. 11. (월) </div>
-      </div>
-    </div>
+          <br /> 23:59:59 접수 마감
+        </SubProcessDetail>
+      </SubProcessFirst>
+      <SubProcessSecond>
+        <SubProcessTitle>02</SubProcessTitle>
+        <SubProcessTitle1>서류 결과 발표</SubProcessTitle1>
+        <SubProcessDetail>2022. 11. 11. (월)</SubProcessDetail>
+      </SubProcessSecond>
+      <SubProcessThird>
+        <SubProcessTitle>03</SubProcessTitle>
+        <SubProcessTitle1>면접</SubProcessTitle1>
+        <SubProcessDetail> 2022. 11. 11. (월) ~ 11. 11. (월)</SubProcessDetail>
+      </SubProcessThird>
+      <SubProcessFourth>
+        <SubProcessTitle>04</SubProcessTitle>
+        <SubProcessTitle1>최종 화이어 발표</SubProcessTitle1>
+        <SubProcessDetail> 2022. 11. 11. (월)</SubProcessDetail>
+      </SubProcessFourth>
+    </SubProcessBg>
   );
 }
 
---
+++ src/Components/Recruit/ProcessPage/ProcessElement.js
@@ -0,0 +1,185 @@
+import styled from "styled-components";
+
+export const SubProcessBg = styled.div`
+  position: absolute;
+  width: 1920px;
+  height: 1080px;
+  left: 0px;
+  top: 0px;
+
+  background: #ffffff;
+`;
+export const SubProcessMainTitle = styled.div`
+  /* Recruiting Process */
+  position: absolute;
+  width: 613px;
+  height: 73px;
+  left: 186.88px;
+  top: 237.75px;
+  margin: 0px;
+
+  font-family: "Roboto Bold";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 61px;
+  line-height: 120%;
+  /* or 73px */
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.2px;
+  text-transform: capitalize;
+
+  /* 헤드라인 */
+
+  color: #333333;
+`;
+export const SubProcessMainDetail = styled.div`
+  position: absolute;
+  width: 505px;
+  height: 154px;
+  left: 187px;
+  top: 361px;
+  margin: 0px;
+
+  /* 18/28 */
+
+  font-family: "Noto Sans Regular";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+  /* or 156% */
+
+  letter-spacing: -0.008em;
+
+  /* 본문 */
+
+  color: #666666;
+`;
+
+export const SubProcessCalendar = styled.img`
+  position: absolute;
+  width: 392.5px;
+  height: 307.5px;
+  left: 84.5px;
+  top: 609.5px;
+`;
+
+export const SubProcessTitle = styled.div`
+  position: absolute;
+  width: 89px;
+  height: 66px;
+  left: 43.5px;
+  top: 42.75px;
+
+  font-family: "Raleway Bold";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 70px;
+  line-height: 96px;
+
+  /* or 137% */
+  font-feature-settings: "pnum" on, "lnum" on;
+
+  /* red */
+  color: #e02f36;
+`;
+
+export const SubProcessTitle1 = styled.div`
+  position: absolute;
+  width: 177px;
+  height: 35.75px;
+  left: 43.5px;
+  top: 223px;
+
+  font-family: "Spoqa Han Sans Neo Bold";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 26px;
+  line-height: 31px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.8px;
+
+  /* 헤드라인 */
+
+  color: #333333;
+`;
+export const SubProcessDetail = styled.div`
+  position: absolute;
+  width: 271.07px;
+  height: 47.25px;
+  left: 43.5px;
+  top: 276.75px;
+
+  font-family: "Spoqa Han Sans Neo Regular";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 150%;
+  /* or 27px */
+
+  letter-spacing: -0.8px;
+  text-transform: capitalize;
+
+  /* Text/Body Text */
+
+  color: #666666;
+`;
+export const SubProcessFirst = styled.div`
+  /* Rectangle 4279 */
+  position: absolute;
+  width: 345px;
+  height: 397px;
+  left: 930.5px;
+  top: 113.25px;
+
+  background: #ffffff;
+  /* 연한 그림자 */
+
+  box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
+  border-radius: 15px;
+`;
+export const SubProcessSecond = styled.div`
+  /* Rectangle 4279 */
+  position: absolute;
+  width: 345px;
+  height: 397px;
+  left: 1311.5px;
+  top: 191.5px;
+
+  /* 화이트 */
+
+  background: #ffffff;
+  box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
+  border-radius: 15px;
+`;
+export const SubProcessThird = styled.div`
+  /* Rectangle 4279 */
+  position: absolute;
+  width: 345px;
+  height: 397px;
+  left: 930.5px;
+  top: 544px;
+
+  /* 화이트 */
+
+  background: #ffffff;
+  box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
+  border-radius: 15px;
+`;
+export const SubProcessFourth = styled.div`
+  /* Rectangle 4279 */
+  position: absolute;
+  width: 345px;
+  height: 397px;
+  left: 1311.5px;
+  top: 622.25px;
+
+  /* 화이트 */
+
+  background: #ffffff;
+  box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
+  border-radius: 15px;
+`;
