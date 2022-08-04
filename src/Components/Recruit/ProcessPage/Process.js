9d94b263a67e7ab097206ffa9547b9ca96c79ca6
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-08-04 23:14:35 +0900

    fix: SubProcess Responsive(임시)

--- src/Components/Recruit/ProcessPage/Process.js
+++ src/Components/Recruit/ProcessPage/Process.js
@@ -12,42 +12,51 @@ import {
   SubProcessTitle1,
   SubProcessDetail,
   SubProcessCalendar,
+  SubProcessWrapper,
+  SubProcessContainer,
 } from "./ProcessElement.js";
 function Process() {
   return (
-    <SubProcessBg>
-      <GlobalFonts />
-      <SubProcessMainTitle>Recruiting Process</SubProcessMainTitle>
-      <SubProcessMainDetail>
-        HWA-E는 6개월 정도의 기간 동안 디자이너와 개발자가 팀을 이루어
-        아이디어를 구체 화하고 프로젝트를 진행합니다. 사업 화보다는 즐겁게
-        서비스를 개발해 보는 경험을 얻는 것을 목적으로 하고 있습니다.
-      </SubProcessMainDetail>
-      <SubProcessCalendar src={calendar} alt="calendar" />
-      <SubProcessFirst>
-        <SubProcessTitle>01</SubProcessTitle>
-        <SubProcessTitle1>서류 접수</SubProcessTitle1>
-        <SubProcessDetail>
-          2022. 11. 11. (월) ~ 11. 11. (월)
-          <br /> 23:59:59 접수 마감
-        </SubProcessDetail>
-      </SubProcessFirst>
-      <SubProcessSecond>
-        <SubProcessTitle>02</SubProcessTitle>
-        <SubProcessTitle1>서류 결과 발표</SubProcessTitle1>
-        <SubProcessDetail>2022. 11. 11. (월)</SubProcessDetail>
-      </SubProcessSecond>
-      <SubProcessThird>
-        <SubProcessTitle>03</SubProcessTitle>
-        <SubProcessTitle1>면접</SubProcessTitle1>
-        <SubProcessDetail> 2022. 11. 11. (월) ~ 11. 11. (월)</SubProcessDetail>
-      </SubProcessThird>
-      <SubProcessFourth>
-        <SubProcessTitle>04</SubProcessTitle>
-        <SubProcessTitle1>최종 화이어 발표</SubProcessTitle1>
-        <SubProcessDetail> 2022. 11. 11. (월)</SubProcessDetail>
-      </SubProcessFourth>
-    </SubProcessBg>
+    <SubProcessContainer>
+      <SubProcessWrapper>
+        <SubProcessBg>
+          <GlobalFonts />
+          <SubProcessMainTitle>Recruiting Process</SubProcessMainTitle>
+          <SubProcessMainDetail>
+            HWA-E는 6개월 정도의 기간 동안 디자이너와 개발자가 팀을 이루어
+            아이디어를 구체 화하고 프로젝트를 진행합니다. 사업 화보다는 즐겁게
+            서비스를 개발해 보는 경험을 얻는 것을 목적으로 하고 있습니다.
+          </SubProcessMainDetail>
+          <SubProcessCalendar src={calendar} alt="calendar" />
+          <SubProcessFirst>
+            <SubProcessTitle>01</SubProcessTitle>
+            <SubProcessTitle1>서류 접수</SubProcessTitle1>
+            <SubProcessDetail>
+              2022. 11. 11. (월) ~ 11. 11. (월)
+              <br /> 23:59:59 접수 마감
+            </SubProcessDetail>
+          </SubProcessFirst>
+          <SubProcessSecond>
+            <SubProcessTitle>02</SubProcessTitle>
+            <SubProcessTitle1>서류 결과 발표</SubProcessTitle1>
+            <SubProcessDetail>2022. 11. 11. (월)</SubProcessDetail>
+          </SubProcessSecond>
+          <SubProcessThird>
+            <SubProcessTitle>03</SubProcessTitle>
+            <SubProcessTitle1>면접</SubProcessTitle1>
+            <SubProcessDetail>
+              {" "}
+              2022. 11. 11. (월) ~ 11. 11. (월)
+            </SubProcessDetail>
+          </SubProcessThird>
+          <SubProcessFourth>
+            <SubProcessTitle>04</SubProcessTitle>
+            <SubProcessTitle1>최종 화이어 발표</SubProcessTitle1>
+            <SubProcessDetail> 2022. 11. 11. (월)</SubProcessDetail>
+          </SubProcessFourth>
+        </SubProcessBg>
+      </SubProcessWrapper>
+    </SubProcessContainer>
   );
 }
 
--- src/Components/Recruit/ProcessPage/ProcessElement.js
+++ src/Components/Recruit/ProcessPage/ProcessElement.js
@@ -1,22 +1,47 @@
 import styled from "styled-components";
-
-export const SubProcessBg = styled.div`
-  position: absolute;
-  width: 1920px;
-  height: 1080px;
-  left: 0px;
-  top: 0px;
-
+export const SubProcessContainer = styled.div`
   background: #ffffff;
+  color: #ffffff;
+  height: 100vh;
+  // width: 100wh;
+  scroll-snap-align: start;
+
+  @media screen and (max-width: 768px) {
+    /* padding: 100px 0; */
+  }
+`;
+export const SubProcessBg = styled.div`
+  height: 100vh;
+  width: 100wh;
+  background: #ffffff;
+`;
+
+export const SubProcessWrapper = styled.div`
+  display: grid;
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+  max-width: 1580px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+  /* padding: 0 24px; */
+  justify-content: center;
+  align-items: center;
 `;
 export const SubProcessMainTitle = styled.div`
   /* Recruiting Process */
   position: absolute;
-  width: 613px;
-  height: 73px;
-  left: 186.88px;
-  top: 237.75px;
-  margin: 0px;
+  // width: 613px;
+  // height: 73px;
+  // left: 186.88px;
+  // top: 237.75px;
+  width: 47.89vw;
+  height: 6vw;
+  left: 9.6vw;
+  top: 15vh;
+  margin: 0;
 
   font-family: "Roboto Bold";
   font-style: normal;
@@ -33,21 +58,32 @@ export const SubProcessMainTitle = styled.div`
   /* 헤드라인 */
 
   color: #333333;
+
+  @media screen and (max-width: 768px) {
+    font-size: 47px;
+    width: 70vw;
+    height: 6vw;
+    left: 9.6vw;
+    top: 13vh;
+  }
 `;
 export const SubProcessMainDetail = styled.div`
   position: absolute;
-  width: 505px;
-  height: 154px;
-  left: 187px;
-  top: 361px;
-  margin: 0px;
-
+  // width: 505px;
+  // height: 154px;
+  // left: 187px;
+  // top: 361px;
+  margin: 0;
+  width: 29vw;
+  height: 8vw;
+  left: 9.9vw;
+  top: 28vh;
   /* 18/28 */
 
   font-family: "Noto Sans Regular";
   font-style: normal;
   font-weight: 400;
-  font-size: 18px;
+  font-size: 17px;
   line-height: 28px;
   /* or 156% */
 
@@ -56,23 +92,41 @@ export const SubProcessMainDetail = styled.div`
   /* 본문 */
 
   color: #666666;
+  @media screen and (max-width: 768px) {
+    font-size: 13px;
+    width: 82vw;
+    height: 6vw;
+    left: 10vw;
+    top: 20vh;
+  }
 `;
 
 export const SubProcessCalendar = styled.img`
   position: absolute;
-  width: 392.5px;
-  height: 307.5px;
-  left: 84.5px;
-  top: 609.5px;
-`;
+  // width: 392.5px;
+  // height: 307.5px;
+  // left: 84.5px;
+  // top: 609.5px;
+  width: 20vw;
+  height: 20vw;
+  left: 5vw;
+  top: 50vh;
 
+  @media screen and (max-width: 768px) {
+    visibility: hidden;
+  }
+`;
+// 모바일에서는 캘린더 이미지 없앰
 export const SubProcessTitle = styled.div`
   position: absolute;
-  width: 89px;
-  height: 66px;
-  left: 43.5px;
-  top: 42.75px;
-
+  // width: 89px;
+  // height: 66px;
+  // left: 43.5px;
+  // top: 42.75px;
+  width: 50vw;
+  height: 10vw;
+  left: 2vw;
+  top: 5vh;
   font-family: "Raleway Bold";
   font-style: normal;
   font-weight: 700;
@@ -84,14 +138,25 @@ export const SubProcessTitle = styled.div`
 
   /* red */
   color: #e02f36;
+  @media screen and (max-width: 768px) {
+    font-size: 60px;
+    width: 50vw;
+    height: 6vw;
+    left: 4vw;
+    top: 2vh;
+  }
 `;
 
 export const SubProcessTitle1 = styled.div`
   position: absolute;
-  width: 177px;
-  height: 35.75px;
-  left: 43.5px;
-  top: 223px;
+  // width: 177px;
+  // height: 35.75px;
+  // left: 43.5px;
+  // top: 223px;
+  width: 50vw;
+  height: 10vw;
+  left: 2vw;
+  top: 15vh;
 
   font-family: "Spoqa Han Sans Neo Bold";
   font-style: normal;
@@ -105,13 +170,24 @@ export const SubProcessTitle1 = styled.div`
   /* 헤드라인 */
 
   color: #333333;
+  @media screen and (max-width: 768px) {
+    font-size: 25px;
+    width: 50vw;
+    height: 6vw;
+    left: 4vw;
+    top: 16vh;
+  }
 `;
 export const SubProcessDetail = styled.div`
   position: absolute;
-  width: 271.07px;
-  height: 47.25px;
-  left: 43.5px;
-  top: 276.75px;
+  // width: 271.07px;
+  // height: 47.25px;
+  // left: 43.5px;
+  // top: 276.75px;
+  width: 50vw;
+  height: 10vw;
+  left: 2vw;
+  top: 30vh;
 
   font-family: "Spoqa Han Sans Neo Regular";
   font-style: normal;
@@ -126,60 +202,107 @@ export const SubProcessDetail = styled.div`
   /* Text/Body Text */
 
   color: #666666;
+  @media screen and (max-width: 768px) {
+    font-size: 15px;
+    width: 50vw;
+    height: 6vw;
+    left: 4vw;
+    top: 23vh;
+  }
 `;
 export const SubProcessFirst = styled.div`
   /* Rectangle 4279 */
   position: absolute;
-  width: 345px;
-  height: 397px;
-  left: 930.5px;
-  top: 113.25px;
-
+  // width: 345px;
+  // height: 397px;
+  // left: 930.5px;
+  // top: 113.25px;
+  width: 18vw;
+  height: 20vw;
+  left: 48.2vw;
+  top: 14.5vh;
+  margin: 0;
   background: #ffffff;
   /* 연한 그림자 */
 
   box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
   border-radius: 15px;
+
+  @media screen and (max-width: 768px) {
+    width: 32vw;
+    height: 36vw;
+    left: 16vw;
+    top: 29vh;
+  }
 `;
 export const SubProcessSecond = styled.div`
   /* Rectangle 4279 */
   position: absolute;
-  width: 345px;
-  height: 397px;
-  left: 1311.5px;
-  top: 191.5px;
-
+  // width: 345px;
+  // height: 397px;
+  // left: 1311.5px;
+  // top: 191.5px;
+  width: 18vw;
+  height: 20vw;
+  left: 68vw;
+  top: 23vh;
   /* 화이트 */
+  margin: 0;
 
   background: #ffffff;
   box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
   border-radius: 15px;
+  @media screen and (max-width: 768px) {
+    width: 32vw;
+    height: 36vw;
+    left: 52vw;
+    top: 30vh;
+  }
 `;
 export const SubProcessThird = styled.div`
   /* Rectangle 4279 */
   position: absolute;
-  width: 345px;
-  height: 397px;
-  left: 930.5px;
-  top: 544px;
-
+  // width: 345px;
+  // height: 397px;
+  // left: 930.5px;
+  // top: 544px;
+  width: 18vw;
+  height: 20vw;
+  left: 48.2vw;
+  top: 62vh;
   /* 화이트 */
+  margin: 0;
 
   background: #ffffff;
   box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
   border-radius: 15px;
+  @media screen and (max-width: 768px) {
+    width: 32vw;
+    height: 36vw;
+    left: 20vw;
+    top: 64vh;
+  }
 `;
 export const SubProcessFourth = styled.div`
   /* Rectangle 4279 */
   position: absolute;
-  width: 345px;
-  height: 397px;
-  left: 1311.5px;
-  top: 622.25px;
-
+  // width: 345px;
+  // height: 397px;
+  // left: 1311.5px;
+  // top: 622.25px;
+  width: 18vw;
+  height: 20vw;
+  left: 68vw;
+  top: 70.5vh;
   /* 화이트 */
-
+  margin: 0;
   background: #ffffff;
   box-shadow: 0px 7px 22px rgba(20, 20, 43, 0.05);
   border-radius: 15px;
+  @media screen and (max-width: 768px) {
+    width: 32vw;
+    height: 36vw;
+    left: 56vw;
+    top: 66vh;
+  }
 `;
