571bfedd06a75baed2fc43a7651cdf51c46b21a9
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-08-04 22:17:38 +0900

    fix: SubHome Responsive(임시)

--- src/Components/Recruit/HomePage/Home.js
+++ src/Components/Recruit/HomePage/Home.js
@@ -1,35 +1,42 @@
-import NavBar from "../NavBar/NavBar.js";
 import hwae_3d from "../../../Images/hwae-3d.svg";
 import RR from "../../../Images/RR.svg";
 import { GlobalFonts } from "../../../fonts/fonts";
 import {
-  SubHomeBg,
   SubHomeTitle,
   SubHomeTitle2,
   SubHomeBtn,
   SubHomeImg,
   SubHomeRR,
+  SubHomeContainer,
+  SubHomeRow,
+  Column1,
+  Column2,
 } from "./HomeElement.js";
 
-function Home() {
+function Home(imgStart) {
   return (
     <>
       <GlobalFonts />
-      <SubHomeBg>
-        <NavBar />
-        <SubHomeTitle>
-          HWA-E <br />
-          2nd Member
-          <br /> Recruiting
-        </SubHomeTitle>
-        <SubHomeTitle2>
-          화이의 두번째 화이어를 모집합니다. <br />
-          화이에서 함께할 분들은 지원 부탁드립니다!
-        </SubHomeTitle2>
-        <SubHomeBtn>Read More</SubHomeBtn>
-        <SubHomeImg src={hwae_3d}></SubHomeImg>
-        <SubHomeRR src={RR}></SubHomeRR>
-      </SubHomeBg>
+      <SubHomeContainer>
+        <SubHomeRow imgStart={imgStart}>
+          <Column1>
+            <SubHomeTitle>
+              HWA-E <br />
+              2nd Member
+              <br /> Recruiting
+            </SubHomeTitle>
+            <SubHomeTitle2>
+              화이의 두번째 화이어를 모집합니다. <br />
+              화이에서 함께할 분들은 지원 부탁드립니다!
+            </SubHomeTitle2>
+            <SubHomeBtn>Read More</SubHomeBtn>
+          </Column1>
+          <Column2>
+            <SubHomeImg src={hwae_3d}></SubHomeImg>
+            <SubHomeRR src={RR}></SubHomeRR>
+          </Column2>
+        </SubHomeRow>
+      </SubHomeContainer>
     </>
   );
 }
--- src/Components/Recruit/HomePage/HomeElement.js
+++ src/Components/Recruit/HomePage/HomeElement.js
@@ -1,57 +1,123 @@
 import styled from "styled-components";
 
-export const SubHomeBg = styled.div`
-  position: absolute;
-  width: 1920px;
-  height: 1080px;
-  left: 0px;
-  top: 0px;
-
+export const SubHomeContainer = styled.div`
   background: #f8f8fa;
+  display: flex;
+  padding: 0 30px;
+  height: 100vh;
+  // width: 100vw;
+  position: relative;
+  z-index: 1;
+  scroll-snap-align: start;
+
+  :before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 3;
+  }
 `;
+
+export const SubHomeRow = styled.div`
+  display: grid;
+  grid-auto-columns: minmax(auto, 1fr);
+  align-items: center;
+  grid-template-areas: ${({ imgStart }) =>
+    imgStart ? `'col2 col1'` : `'col1 col2'`};
+
+  @media screen and (max-width: 768px) {
+    grid-template-areas: ${({ imgStart }) =>
+      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
+  }
+`;
+
+export const Column1 = styled.div`
+  margin-bottom: 15px;
+  padding: 0 15px;
+  grid-area: col1;
+`;
+
+export const Column2 = styled.div`
+  margin-bottom: 15px;
+  padding: 0 15px;
+  grid-area: col2;
+`;
+
 export const SubHomeTitle = styled.div`
   position: absolute;
-  width: 699px;
-  height: 348px;
-  left: 185px;
-  top: 225px;
-
+  // width: 699px;
+  // height: 348px;
+  //   left: 185px;
+  //   top: 225px;
+  width: 55vw;
+  height: 27vw;
+  left: 13vw;
+  top: 18vh;
   font-family: "Roboto Bold";
   font-style: normal;
   font-weight: 700;
   font-size: 96px;
   line-height: 105%;
-
-  margin: 0px;
+  margin: 0;
 
   color: #333333;
+  // @media screen and (max-width: 1000px) {
+  //   font-size: 80px;
+  //   width: 70vw;
+  //   height: 20vw;
+  //   left: 14vw;
+  //   top: 10vh;
+  // }
+  @media screen and (max-width: 768px) {
+    font-size: 75px;
+    width: 70vw;
+    height: 20vw;
+    left: 18vw;
+    top: 12vh;
+  }
 `;
 export const SubHomeTitle2 = styled.div`
   position: absolute;
-  width: 457px;
-  height: 72px;
-  left: 190px;
-  top: 573px;
+  // width: 457px;
+  // height: 72px;
+  // left: 190px;
+  // top: 573px;
+  width: 35.703125vw;
+  height: 7px;
+  left: 13.2vw;
+  top: 55vh;
 
   font-family: "Noto Sans Regular";
   font-style: normal;
   font-weight: 400;
-  font-size: 22px;
+  font-size: 20px;
   line-height: 150%;
 
   letter-spacing: -0.008em;
-  margin: 0px;
-
   color: #666666;
+
+  @media screen and (max-width: 768px) {
+    font-size: 17px;
+    width: 70vw;
+    height: 20vw;
+    left: 19vw;
+    top: 40vh;
+  }
 `;
 
 export const SubHomeBtn = styled.div`
   position: absolute;
-  left: 9.95%;
-  right: 82.55%;
-  top: 62.96%;
-  bottom: 32.59%;
-
+  // width: 144px;
+  // height: 48px;
+  // left: 191px;
+  // top: 680px;
+  width: 8vw;
+  height: 2.5vw;
+  left: 13.3vw;
+  top: 65vh;
   background: #ff2d46;
   box-shadow: 0px 2.80141px 11.2057px rgba(255, 45, 70, 0.2);
   border-radius: 52.293px;
@@ -66,18 +132,48 @@ export const SubHomeBtn = styled.div`
   align-items: center;
 
   color: #ffffff;
+
+  @media screen and (max-width: 768px) {
+    font-size: 13px;
+    width: 15vw;
+    height: 5vw;
+    left: 70vw;
+    top: 41vh;
+  }
 `;
+
 export const SubHomeImg = styled.img`
   position: absolute;
-  width: 1160px;
-  height: 845px;
-  left: 785px;
-  top: 57px;
+  // width: 1160px;
+  // height: 845px;
+  // left: 785px;
+  // top: 57px;
+  width: 90.625vw;
+  height: 40vw;
+  left: 27vw;
+  top: 4vh;
+  @media screen and (max-width: 768px) {
+    width: 90vw;
+    height: 80vw;
+    left: 6vw;
+    top: 38vh;
+  }
 `;
 export const SubHomeRR = styled.img`
   position: absolute;
-  left: 48.96%;
-  right: 48.96%;
-  top: 90.65%;
-  bottom: 5.65%;
+  // width: 40px;
+  // height: 40px;
+  // left: 940px;
+  // top: 959px;
+  width: 2vww;
+  height: 2vw;
+  left: 50vw;
+  top: 90vh;
+
+  @media screen and (max-width: 768px) {
+    width: 4vw;
+    height: 4vw;
+    left: 50vw;
+    top: 94vh;
+  }
 `;
