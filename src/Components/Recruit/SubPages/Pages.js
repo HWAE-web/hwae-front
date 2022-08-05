872d88d5047957a5df2b04acae47a5e81231d36c
Lee Haewon <94354545+Haewonny@users.noreply.github.com>
2022-08-05 20:27:59 +0900

    feat: SubPage scroll

---
+++ src/Components/Recruit/SubPages/Pages.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { GlobalFonts } from "../../../fonts/fonts";
+import Home from "../HomePage/Home";
+import Process from "../ProcessPage/Process";
+import FAQ from "../FAQPage/FAQ";
+import Recruit from "../RecruitPage/Recruit";
+
+import SubNavBar from "../NavBar/NavBar";
+import { PageContainer } from "./PagesElement";
+
+const SubPage = () => {
+  return (
+    <PageContainer>
+      <GlobalFonts />
+      <SubNavBar />
+      <Home />
+      <Process />
+      <Recruit />
+      {/* <FAQ /> */}
+    </PageContainer>
+  );
+};
+
+export default SubPage;
