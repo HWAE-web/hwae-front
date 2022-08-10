41d4a7d77822fda607a863263dce25e31c16a53e
Haewonny <hawon2205@ewhain.net>
2022-08-10 22:08:43 +0900

    refactor: use BrowserRouter(index.js)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router } from "react-router-dom";
 import MainHome from "./Components/Main/pages";
+import SubPage from "./Components/Recruit/SubPages/Pages"
 
 function App() {
-  return (
-    <Router>
-      <MainHome />
-    </Router>
+  return (  
+      // <SubPage/>
+      <MainHome/>
   );
 }
 
 export default App;
 
 // App.js는 상대꺼 주석처리하고 걍 쓰는걸로.
+// index.js에서 BrowserRouter 써서 여기서 라우터 안 써도 됨!
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalFonts } from "./fonts/fonts";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalFonts />
-    <App />
+    <BrowserRouter>
+      <GlobalFonts />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
